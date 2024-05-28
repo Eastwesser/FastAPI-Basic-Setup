a35ecf3f708df981b177bacddf1efff2bbf21c26
Dennis Mattews <alttert@mail.ru>
2024-05-29 01:29:03 +0300

    fast_api_app_5 created api router

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -12,4 +12,9 @@ app.include_router(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
