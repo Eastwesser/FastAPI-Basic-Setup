5b005e8ecc57ccea74e1a9880cb6b44bf947247b
Dennis Mattews <alttert@mail.ru>
2024-05-29 01:22:47 +0300

    fast_api_app_4 created core config

---
+++ fastapi-application/core/config.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+from pydantic_settings import BaseSettings
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+
+# Used for all settings:
+settings = Settings()
