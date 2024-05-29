58fa0559855cd626c44b2e81da0594b9320f09b1
Dennis Mattews <alttert@mail.ru>
2024-05-29 23:43:18 +0300

    config read from env

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel
-from pydantic import PostgresDsn
+import os
 
-from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pydantic import (
+    BaseModel,
+    PostgresDsn,
+)
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+load_dotenv()
 
 
 class RunConfig(BaseModel):
@@ -13,19 +22,27 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+db_url = os.getenv("FASTAPI_CONFIG__DB__URL")
+
+
 class DatabaseConfig(BaseModel):
-    url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    pool_size: int = 50,
-    max_overflow: int = 10,
+    url: PostgresDsn = db_url
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env-template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    db: DatabaseConfig
+    db: DatabaseConfig = DatabaseConfig()
 
 
-# Used for all settings:
 settings = Settings()
