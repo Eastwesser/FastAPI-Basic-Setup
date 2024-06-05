35c795d963c98af7a8bc27e5dfb4acd41e40bc78
Dennis Mattews <alttert@mail.ru>
2024-06-05 21:11:20 +0300

    users in crud changed

--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -1,16 +1,37 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
-from core.schemas.user import UserRead
-from crud.users import get_all_users
+from core.schemas.user import UserRead, UserCreate
+from crud import users as crud_users
 
 router = APIRouter(tags=["Users"])
 
 
 @router.get("", response_model=list[UserRead])
 async def get_users(
-        session: AsyncSession = Depends(db_helper.session_getter),
+        # session: AsyncSession = Depends(db_helper.session_getter),
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter)
+        ],
 ):
-    users = await get_all_users(session=session)
+    users = await crud_users.get_all_users(session=session)
     return users
+
+
+@router.post("", response_model=UserRead)
+async def create_user(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter)
+        ],
+        user_create: UserCreate,
+):
+    user = await crud_users.create_user(
+        session=session,
+        user_create=user_create,
+    )
+    return user
--- fastapi-application/crud/users.py
+++ fastapi-application/crud/users.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Donut
+from core.schemas.user import UserCreate
 
 
 async def get_all_users(
@@ -12,3 +13,14 @@ async def get_all_users(
     stmt = select(Donut).order_by(Donut.id)
     result = await session.execute(stmt)
     return result.scalars().all()
+
+
+async def create_user(
+        session: AsyncSession,
+        user_create: UserCreate,
+) -> Donut:
+    user = Donut(**user_create.model_dump())
+    session.add(user)
+    await session.commit()
+    # await session.refresh(user)
+    return user
